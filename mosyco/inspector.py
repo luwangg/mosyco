293c73073485b3d919f498dffb88406253800caf
vab9 <vbrinkhege@uos.de>
2017-07-31 16:44:03 +0200

    replace str(next_year) w/ pd.Period

--- mosyco/inspector.py
+++ mosyco/inspector.py
@@ -123,10 +123,13 @@ class Inspector:
         # ======================================================================
 
         # variable data has model and relevant forecasting data for period
-        # this will raise an exception if  we haven't forecast the required period yet
+        # this will raise an exception if we haven't forecast the required period yet
         try:
             data = pd.concat(
-                [self.df.loc[str(period)], self.forecast.loc[str(period)]],
+                [
+                self.df.loc[period.start_time:period.end_time],
+                self.forecast.loc[period.start_time:period.end_time]
+                ],
                 axis=1)
         except KeyError as e:
             raise KeyError(f"Forecasting data for {period} not available.")
@@ -235,11 +238,11 @@ class Inspector:
         # TODO: entries for period MUST be empty, else this will raise an exception?!
         # double check / assert here that period is in df
         assert 'ds' in self.df.columns
-        fc_frame = self.df.loc[str(period)].filter(['ds'])
+        fc_frame = self.df.loc[period.start_time:period.end_time].filter(['ds'])
 
         # EXPENSIVE - CAN TAKE VERY LONG
-        with helpers.silence():
-            new_forecast = self.forecasting_model.predict(fc_frame)
+
+        new_forecast = self.forecasting_model.predict(fc_frame)
         del fc_frame
 
         # new_forecast needs to have DateTimeIndex
